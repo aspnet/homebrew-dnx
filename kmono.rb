314ed95517334185b3948ef62fa6df474a99f22c
Louis DeJardin <lodejard@microsoft.com>
2014-07-15 12:43:21 -0700

    Binaries moved to aspnet/k release

--- kmono.rb
+++ kmono.rb
@@ -6,7 +6,7 @@ class Kmono < Formula
   version "3.6.1"
 
   bottle do
-    root_url "https://github.com/lodejard/homebrew-k/releases/download/v1.0.0-alpha3-10141"
+    root_url "https://github.com/aspnet/homebrew-k/releases/download/v1.0.0-alpha3-10141"
     sha1 "36ce327e4587f747c6af5bc9c6c45c44634d956b" => :mavericks
   end
 
