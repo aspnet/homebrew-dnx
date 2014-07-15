9fc588bef5184fff02f6329b22152f75bceec017
Louis DeJardin <lodejard@microsoft.com>
2014-07-15 12:38:33 -0700

    symlinking to kimono brew location specifically
    helps k run on machines with standard mono on path

--- kre.rb
+++ kre.rb
@@ -4,7 +4,7 @@ class Kre < Formula
   homepage "https://www.github.com/aspnet/Home"
   version "1.0.0-alpha3-10141"
   url "https://github.com/aspnet/homebrew-k/releases/download/v1.0.0-alpha3-10141/KRE-mono45-x86-1.0.0-alpha3.10141.nupkg"
-  sha1 "dcce1ba6ae6f94a0e8b0f0e6d95559fb7c0cec2a"
+  sha1 "c497f449bb7496dc7e2d41f248f83259b6d4313c"
 
   depends_on "kmono" => :recommended
 
@@ -13,6 +13,7 @@ class Kre < Formula
     (libexec + "klr").chmod 0755
     (libexec + "kpm").chmod 0755
     (libexec + "k").chmod 0755
+    (libexec + "mono").make_symlink "../../../kmono/3.6.1/bin/mono"
     bin.install_symlink "#{libexec}/klr"
     bin.install_symlink "#{libexec}/kpm"
     bin.install_symlink "#{libexec}/k"
