18d6468f0d84869735449448d40766d94065fab0
Suhas Joshi <suhasbjoshi@hotmail.com>
2014-09-24 13:58:54 -0700

    Updated to install from Home repo

--- kvm.rb
+++ kvm.rb
@@ -3,12 +3,12 @@ require "formula"
 class Kvm < Formula
   homepage "https://www.github.com/aspnet/Home"
   version "1.0.0-alpha4-1"
-  url "https://github.com/aspnet/kvm.git", :branch => 'feature-homebrew'
+  url "https://github.com/aspnet/Home.git", :branch => 'master'
 
   depends_on "kmono" => :recommended
 
   def install
-    libexec.install "src/kvm.sh"
+    libexec.install "kvm.sh"
     (libexec + "kvm.sh").chmod 0755
     (libexec + "mono").make_symlink Formula["kmono"].opt_bin/"mono"
     system "bash -c 'source #{libexec}/kvm.sh; kvm upgrade'"
