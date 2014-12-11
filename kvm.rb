85d26477bd62e0d36f8607807224dad6fa033892
anurse <Andrew Stanton-Nurse>
2014-12-10 20:42:29 -0800

    Updated kvm homebrew
    * Removed old kmono and kre formulae (are they still needed?)
    * Changed kvm to use the aspnet/kvm repo

--- kvm.rb
+++ kvm.rb
@@ -1,14 +1,21 @@
 require "formula"
 
 class Kvm < Formula
-  homepage "https://www.github.com/aspnet/Home"
+  homepage "https://www.github.com/aspnet/kvm"
   version "1.0.0-beta1"
-  url "https://github.com/aspnet/Home.git", :branch => 'master'
+
+  stable do
+    url "https://github.com/aspnet/kvm.git", :branch => 'master'
+  end
+
+  head do
+    url "https://github.com/aspnet/kvm.git", :branch => 'dev'
+  end
 
   depends_on "mono" => :recommended
 
   def install
-    libexec.install "kvm.sh"
+    libexec.install "src/kvm.sh"
     (libexec + "kvm.sh").chmod 0755
     (libexec + "mono").make_symlink Formula["mono"].opt_bin/"mono"
     system "bash -c 'source #{libexec}/kvm.sh; kvm upgrade'"
