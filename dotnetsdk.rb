cd0cc8021142f3a1d9f094c5cadbc329034cc594
anurse <andrew@andrewnurse.net>
2015-01-22 14:49:20 -0800

    temporarily switch stable build to release branch

--- dotnetsdk.rb
+++ dotnetsdk.rb
@@ -9,7 +9,11 @@ class Dotnetsdk < Formula
   end
 
   stable do
-    url "https://github.com/aspnet/Home.git", :branch => 'v1.0.0-beta3'
+    # TEMPORARY until beta3 is released and there is a formal tag for the latest version available
+    url "https://github.com/aspnet/Home.git", :branch => 'release'
+
+    # Switch this to the below once beta3 is released and the tag is made
+    #url "https://github.com/aspnet/Home.git", :tag => 'v1.0.0-beta3'
   end
 
   depends_on "mono" => :recommended
@@ -18,7 +22,7 @@ class Dotnetsdk < Formula
     libexec.install "dotnetsdk.sh"
     (libexec + "dotnetsdk.sh").chmod 0755
     (libexec + "mono").make_symlink Formula["mono"].opt_bin/"mono"
-    system "bash -c 'source #{libexec}/dotnetsdk.sh; kvm upgrade'"
+    system "bash -c 'source #{libexec}/dotnetsdk.sh; dotnetsdk upgrade'"
     bin.install_symlink "#{libexec}/dotnetsdk.sh"
   end
 
--- kvm.rb
+++ kvm.rb
@@ -4,27 +4,16 @@ class Kvm < Formula
   homepage "https://www.github.com/aspnet/Home"
   version "1.0.0-beta2"
 
-  url "https://github.com/aspnet/Home.git", :branch => 'v1.0.0-beta2'
+  url "https://github.com/aspnet/Home.git", :tag => 'v1.0.0-beta2'
 
   depends_on "mono" => :recommended
 
   def install
-    opoo "The 'kvm' tool has been deprecated. Install 'dotnetsdk' for the latest versions of the .NET Cross-Platform SDK"
-    libexec.install "kvm.sh"
-    (libexec + "kvm.sh").chmod 0755
-    (libexec + "mono").make_symlink Formula["mono"].opt_bin/"mono"
-    system "bash -c 'source #{libexec}/kvm.sh; kvm upgrade'"
-    bin.install_symlink "#{libexec}/kvm.sh"
-    bin.install_symlink "#{libexec}/current/k"
-    bin.install_symlink "#{libexec}/current/klr"
-    bin.install_symlink "#{libexec}/current/kpm"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to the ~/.bash_profile, ~/.bashrc or ~/.zshrc file:
-
-      source kvm.sh
-
-    EOS
+    onoe "The 'kvm' tool has been deprecated. Install 'dotnetsdk' for the latest versions of the .NET Cross-Platform SDK"
+    onoe "Run the following commands:"
+    onoe " > brew remove kvm"
+    onoe " > brew install dotnetsdk"
+    onoe "Then update your ~/.bashrc, ~/.bash_profile, and ~/.zshrc files as needed"
+    onoe "See https://github.com/aspnet/Home for more info!"
   end
 end
